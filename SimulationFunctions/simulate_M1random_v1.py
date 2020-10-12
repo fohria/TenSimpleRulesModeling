5182bf0c84de8d84b4bfb9cbd875d40e840753fe
foh <foh@sanipia.se>
2020-10-12 14:51:53 +0100

    added docstring

--- SimulationFunctions/simulate_M1random_v1.py
+++ SimulationFunctions/simulate_M1random_v1.py
@@ -1,17 +1,38 @@
 import numpy as np
 from numba import njit, int32
 
-# from SimulationFunctions.choose import choose  # temp when working in top dir
-from .choose import choose  # when using as submodule from top functions
+from .choose import choose
 
 
 @njit
 def simulate_M1random_v1(trial_count, bandit, bias):
+    """
+    simulates a participant making random choices with bias for one choice.
+
+    parameters
+    ----------
+    trial_count : int
+        number of trials, i.e. how many times do we pull a bandit arm
+    bandit : numpy array of length 2
+        represents a multi armed bandit. its length is the number of arms
+        (NOTE: can only handle 2 arms for now) and each array item represents
+        the probability of reward for pulling that bandit arm.
+    bias : float (0-1)
+        the bias towards the leftmost (first array position) choice
+
+    returns
+    -------
+    actions : numpy array with length trial_count
+        sequence of actions taken (0 or 1 for 2 arm bandit) on each trial
+    rewards : numpy array with length trial_count
+        sequence of rewards received on each trial
+    """
 
     actions = np.zeros(trial_count, dtype=int32)
     rewards = np.zeros(trial_count, dtype=int32)
 
-    probabilities = np.array([bias, 1 - bias])  # b never changes
+    # probabilities don't change between trials
+    probabilities = np.array([bias, 1 - bias])
 
     for trial in range(trial_count):
 
